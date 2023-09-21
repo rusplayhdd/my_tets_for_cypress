5ab7bddd17163cc03d24bfe6f0319c32793d4d50
rusplay <nikolaenkoruslan011@gmail.com>
2023-09-21 10:00:47 +0300

    update conf

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     viewportHeight: 768,
     viewportWidth: 1366,
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
