0b7c3780dd2598cefa3bc44b90d5203422b298ae
rus play <nikolaenkoruslan011@gmail.com>
2023-11-20 19:09:03 +0300

    added a new e2e test!!! refreshed!

--- cypress/e2e/my_light_test.cy.js
+++ cypress/e2e/my_light_test.cy.js
@@ -1,36 +1,36 @@
 
 
-// describe('test an order', () => {
-//   it('needed to choose', () => {
-//     cy.visit('https://staya.dog/').contains('DAWGS FOR DOGS');
+    // describe('test an order', () => {
+    //   it('needed to choose', () => {
+    //     cy.visit('https://staya.dog/').contains('DAWGS FOR DOGS');
 
-//     cy.get('.CookieNotification__button > .s-button__content').click();
-//     cy.get('[href="/catalog/leashes"] > .category-link__canvas-wrapper > .category-link__canvas').click();
-//     cy.wait(6500);
-//     cy.contains('Стандартные поводки');
+    //     cy.get('.CookieNotification__button > .s-button__content').click();
+    //     cy.get('[href="/catalog/leashes"] > .category-link__canvas-wrapper > .category-link__canvas').click();
+    //     cy.wait(6500);
+    //     cy.contains('Стандартные поводки');
 
-//     cy.get(':nth-child(1) > .SBubble__bubble-wrapper > .SBubble__bubble > .picture-wp > .picture-wp__img').click();
-//     cy.get(':nth-child(2) > .product-list > :nth-child(1) > .SProductCard__galleryWrapper > .CurtainGallery').click();
-//     cy.contains('STUDIO 21');
-    
-//     var btn0 = ':nth-child(6) > .SSpoiler > .SSpoiler__container > .SRadioButton_checked'
+    //     cy.get(':nth-child(1) > .SBubble__bubble-wrapper > .SBubble__bubble > .picture-wp > .picture-wp__img').click();
+    //     cy.get(':nth-child(2) > .product-list > :nth-child(1) > .SProductCard__galleryWrapper > .CurtainGallery').click();
+    //     cy.contains('STUDIO 21');
+        
+    //     var btn0 = ':nth-child(6) > .SSpoiler > .SSpoiler__container > .SRadioButton_checked'
 
-//     cy.get('.CTA').click();
-//       if(cy.contains('Сначала выберите')) {
-//         cy.get('.SSpoiler__container > :nth-child(1)').click()
-//       };
-//       if(cy.get(btn0 == ':nth-child(6) > .SSpoiler > .SSpoiler__container > .SRadioButton_checked') 
-//         && cy.get('.SRadioButton_disabled' == '.SRadioButton_disabled')) {
-//         cy.get(btn0).click();
-//       };
-    
-    
-//     cy.get('.CTA').click();
-//     cy.contains("STUDIO 21");
-//     cy.get('#__layout > div > section > section > footer > div.cart-modal__actions > a').click();
-//     cy.contains("Оформление заказа");
-    
+    //     cy.get('.CTA').click();
+    //       if(cy.contains('Сначала выберите')) {
+    //         cy.get('.SSpoiler__container > :nth-child(1)').click()
+    //       };
+    //       if(cy.get(btn0 == ':nth-child(6) > .SSpoiler > .SSpoiler__container > .SRadioButton_checked') 
+    //         && cy.get('.SRadioButton_disabled' == '.SRadioButton_disabled')) {
+    //         cy.get(btn0).click();
+    //       };
+        
+        
+    //     cy.get('.CTA').click();
+    //     cy.contains("STUDIO 21");
+    //     cy.get('#__layout > div > section > section > footer > div.cart-modal__actions > a').click();
+    //     cy.contains("Оформление заказа");
+        
 
 
-//   })
-// })
+    //   })
+    // })
